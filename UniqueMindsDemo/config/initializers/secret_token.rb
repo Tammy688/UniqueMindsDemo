e2c596476cd853a4830cffd2388add01d99e04f4
TammyLLC <me@TammyLLC.com>
2014-03-21 11:58:57 -0400

    rails new, better error

---
+++ UniqueMindsDemo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UniqueMindsDemo::Application.config.secret_key_base = 'dac9362e4015ad0a2751a40b1480290da362f4103000679df7fc140cbfe52c362bdd97f27322a6149586dbc30c4a3134d5ac6fdc22ce422910b273814c29aff9'
